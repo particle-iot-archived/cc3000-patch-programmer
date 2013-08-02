438d3471e356fde8f31213892be4fba17a32aee3
Zachary Crockett <towynlin@gmail.com>
2013-08-02 15:51:16 -0500

    Add LED3 and LED4 to Led_TypeDef enum

--- inc/hw_config.h
+++ inc/hw_config.h
@@ -19,7 +19,7 @@
 /* Exported types ------------------------------------------------------------*/
 typedef enum
 {
-	LED1 = 0, LED2 = 1
+	LED1 = 0, LED2 = 1, LED3 = 2, LED4 = 3
 } Led_TypeDef;
 
 typedef enum
